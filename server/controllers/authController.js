926ea9ce2fbb6c6c1300390a5425804c4cb94af9
Anna Duong <aduong9417@gmail.com>
2024-02-27 14:16:59 -0800

    implement route handlers for user login and registration, authController for users, and set up catch all route handler and global error handler

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,1 +1,71 @@
-// require model 
+const pool = require("../models/user");
+const bcrypt = require('bcrypt');
+const authController = {};
+
+function errorCreator(funcName, error) {
+  return {
+    log: `Express error handler caught ${funcName} error with message ${error.message}`, 
+    status: 400, 
+    message: { err: error.message },
+  };
+}
+
+authController.checkUser = async (req, res, next) => {
+  try {
+    const { email } = req.body;
+    const checkUser = `SELECT * FROM public.users WHERE email = $1;`;
+    const { rows } = await pool.query(checkUser, [ email ]);
+
+    if (!rows.length) {
+      return next();
+    } else {
+      return res.status(200).json({ success: false });
+    }
+
+  } catch (error) {
+    return next(errorCreator('checkUser', error));
+  }
+}
+
+authController.getUser = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+    const queryString = `SELECT * FROM public.users WHERE email = $1;`;
+    const { rows } = await pool.query(queryString, [ email ]);
+
+    console.log(rows[0].password);
+
+    if (rows.length) {
+      const valid = await bcrypt.compare(password, rows[0].password);
+      console.log(valid);
+
+      if (valid) {
+        res.locals.userId = rows[0]._id;
+      }
+    }
+    return next();
+
+  } catch (error) {
+    return next(errorCreator('getUser', error));
+  }
+}
+
+authController.registerUser = async (req, res, next) => {
+  try {
+    const { first_name, last_name, email, password } = req.body;
+    const salt = await bcrypt.genSalt();
+    const pwHash = await bcrypt.hash(password, salt);
+    const insertUser = `INSERT INTO public.users (first_name, last_name, email, password) VALUES ($1, $2, $3, $4) RETURNING *;`;
+    const params = [ first_name, last_name, email, pwHash ];
+    const { rows } = await pool.query(insertUser, params);
+    if (rows.length) {
+      res.locals.userId = rows[0]._id;
+    }
+
+    return next();
+  } catch (error) {
+    return next(errorCreator('registerUser', error));
+  }
+}
+
+module.exports = authController;
--- server/server.js
+++ server/server.js
@@ -17,11 +17,41 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-// route handlers 
+// route handlers for users
+app.post('/login', authController.getUser, (req, res) => {
+  return res.status(201).json({ userId: res.locals.userId });
+});
+
+app.post('/register', authController.checkUser, authController.registerUser, authController.getUser, (req, res) => {
+  return res.status(201).json({ userId: res.locals.userId });
+});
+
+// route handlers for reservations
+// app.post('/reservation', authController.postReservation, (req, res) => {
+//   return res.status(201).json({ reservationId: res.locals.reservationId });
+// });
+
+// app.delete('/reservation', authController.deleteReservation, (req, res) => {
+//   return res.status(200).send(`Your reservation for ${res.locals.dateTime} has been cancelled`);
+// })
 
 // catch-all route handler 
+app.use('*', (req, res) => {
+  res.status(404).send('Page not found');
+});
 
 // global error handler 
+app.use((err, req, res, next) => {
+  const defaultErr = {
+    log: 'Express error handler caught unknown middleware error',
+    status: 500,
+    message: { err: 'An error occurred' }
+  };
+
+  const errorObj = Object.assign({}, defaultErr, err);
+  return res.status(errorObj.status).json(errorObj.message);
+});
+
 
 app.listen(PORT, () => {
   console.log(`Server is listening on PORT ${PORT}`);

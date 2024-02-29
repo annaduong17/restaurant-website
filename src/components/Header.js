4b19deb3c779528d033d0b8ff2e2de58e422f8d5
Anna Duong <aduong9417@gmail.com>
2024-02-28 17:27:50 -0800

    add hero image as background-image property of app instead of img element in Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,13 +4,10 @@ import Button from './Button';
 function Header() {
   return (
     <div className='header'>
-      <img className='header-hero' src="/images/homepage/hero-bg-desktop.jpg" alt="plate of pasta" />
-      <div className='header-content'>
-        <DineLogo />
-        <h1>Exquisite dining since 1989</h1>
-        <p>Experience our seasonal menu in beautiful country surroundings. Eat the freshest produce from the comfort of our farmhouse.</p>
-        <Button />
-      </div>
+      <DineLogo />
+      <h1>Exquisite dining since 1989</h1>
+      <p>Experience our seasonal menu in beautiful country surroundings. Eat the freshest produce from the comfort of our farmhouse.</p>
+      <Button />
     </div>
   );
 }

114420a291228fd665025d48b3ce8c46289eb399
Anna Duong <aduong9417@gmail.com>
2024-03-02 20:36:09 -0800

    style footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,19 +2,19 @@ import DineLogo from "./DineLogo";
 
 function Footer() {
   return (
-    <div>
+    <div className="footer">
       <section>
         <DineLogo />
       </section>
 
-      <section>
-        <address>MARTHWAITE, SEDBERGH</address>
-        <address>CUMBRIA</address>
-        <address>+00 44 123 4567</address>
-      </section>
+      <address className="address-section">
+        <p>MARTHWAITE, SEDBERGH</p>
+        <p>CUMBRIA</p>
+        <p>+00 44 123 4567</p>
+      </address>
 
-      <section>
-        <p>Open Times</p>
+      <section className="hours-section">
+        <p>OPEN TIMES</p>
         <p>MON - FRI: 09:00 AM - 10:00 PM</p>
         <p>SAT - SUN: 09:00 AM - 11:30 PM</p>
       </section>

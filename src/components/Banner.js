eb8264b3292cf80eac04e8ce5cb27648eafa4285
Anna Duong <aduong9417@gmail.com>
2024-03-02 20:13:37 -0800

    style banner

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -2,10 +2,11 @@ import Button from "./Button";
 
 function Banner() {
   const imgDesktop = "/images/homepage/ready-bg-desktop.jpg";
+  const imgTablet = "/images/homepage/ready-bg-tablet.jpg";
   const imgMobile = "/images/homepage/ready-bg-mobile.jpg";
 
   return (
-    <div>
+    <div className="banner">
       <img src={imgDesktop} alt="bowl of food" />
       <h3>Ready to make a reservation?</h3>
       <Button />

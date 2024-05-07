df272b98bb07e9ce343ce437ae441de408b5a1a7
Anna Duong <aduong9417@gmail.com>
2024-05-06 22:31:07 -0700

    highlight event link when active and on hover

--- src/components/EventNavLinks.js
+++ src/components/EventNavLinks.js
@@ -1,19 +1,20 @@
 import { NavLink } from 'react-router-dom';
 
 function EventNavLinks({ activeEvent, toggleActiveEvent }) {
+  
   return (
     <div className='event-navlinks'>
       <div>
         {activeEvent === "FAMILY GATHERING" && <span className='first'></span>}
-        <NavLink onClick={toggleActiveEvent} className={`link ${activeEvent === "FAMILY GATHERING" ? "eventActive" : ""}`}>FAMILY GATHERING</NavLink>
+        <NavLink onClick={toggleActiveEvent} className={`link ${activeEvent === "FAMILY GATHERING" ? "darken" : ""}`}>FAMILY GATHERING</NavLink>
       </div>
       <div>
       {activeEvent === "SPECIAL EVENTS" && <span className='second'></span>}
-        <NavLink onClick={toggleActiveEvent} className={`link ${activeEvent === "SPECIAL EVENTS" ? "eventActive" : ""}`}>SPECIAL EVENTS</NavLink>
+        <NavLink onClick={toggleActiveEvent} className={`link ${activeEvent === "SPECIAL EVENTS" ? "darken" : ""}`}>SPECIAL EVENTS</NavLink>
       </div>
       <div> 
       {activeEvent === "SOCIAL EVENTS" && <span className='third'></span>}
-        <NavLink onClick={toggleActiveEvent} className={`link ${activeEvent === "SOCIAL EVENTS" ? "eventActive" : ""}`}>SOCIAL EVENTS</NavLink>
+        <NavLink onClick={toggleActiveEvent} className={`link ${activeEvent === "SOCIAL EVENTS" ? "darken" : ""}`}>SOCIAL EVENTS</NavLink>
       </div>
     </div>
   );

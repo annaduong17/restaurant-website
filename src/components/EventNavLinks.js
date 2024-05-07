26227eb353c218079712ea9f2e2c027581310be6
Anna Duong <aduong9417@gmail.com>
2024-05-06 22:10:35 -0700

    show correct span depending on the event chosen

--- src/components/EventNavLinks.js
+++ src/components/EventNavLinks.js
@@ -4,15 +4,15 @@ function EventNavLinks({ activeEvent, toggleActiveEvent }) {
   return (
     <div className='event-navlinks'>
       <div>
-        <span></span>
+        {activeEvent === "FAMILY GATHERING" && <span className='first'></span>}
         <NavLink onClick={toggleActiveEvent} className={`link ${activeEvent === "FAMILY GATHERING" ? "eventActive" : ""}`}>FAMILY GATHERING</NavLink>
       </div>
       <div>
-      <span></span>
+      {activeEvent === "SPECIAL EVENTS" && <span className='second'></span>}
         <NavLink onClick={toggleActiveEvent} className={`link ${activeEvent === "SPECIAL EVENTS" ? "eventActive" : ""}`}>SPECIAL EVENTS</NavLink>
       </div>
       <div> 
-      <span></span>
+      {activeEvent === "SOCIAL EVENTS" && <span className='third'></span>}
         <NavLink onClick={toggleActiveEvent} className={`link ${activeEvent === "SOCIAL EVENTS" ? "eventActive" : ""}`}>SOCIAL EVENTS</NavLink>
       </div>
     </div>
